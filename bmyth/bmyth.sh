19ac678d718477f8fb1a3f49188a3d290e99b209
spmorton <mortons@comcast.net>
2013-07-20 19:43:25 -0500

    cleaned up comments and debug code

--- bmyth/bmyth.sh
+++ bmyth/bmyth.sh
@@ -5,18 +5,15 @@
 # Set LIRC to execute this with the power buttom or button of your choice.
 
 declare -i DTIME=$(date '+%s')			# get current time
-declare -i BMYTHTIME=$(cat ~/.bmythtime)	# get previous time form file
+declare -i BMYTHTIME=$(cat ~/.bmythtime)	# get previous time from file
 
-#echo $DTIME, $BMYTHTIME
 
 DIF=$(($DTIME - $BMYTHTIME))			# calculate difference
 
-#echo $DIF
 
 if [ $DIF -gt 10 ] || [ $DIF -eq 0 ]; then	# if eq 0 then this is first use
 
   echo $DTIME >~/.bmythtime			# write current time stamp to file
-  #echo in loop
   PIDS='100'
 
   while [ "$PIDS" != "" ]			# had an issue with multiple copies of the FE running
@@ -24,13 +21,9 @@ if [ $DIF -gt 10 ] || [ $DIF -eq 0 ]; then	# if eq 0 then this is first use
 	  PIDS=$(pidof mythfrontend)		# check to see if MFE is running
 	  pkill mythfrontend			# Kill it
 	  sleep 5				# wait 5 seconds
-	  #echo $PIDS
     done
 
-
-else
-  #echo exit loop
-  exit 0					# else force exit with status 0
+  mythfrontend
 
 fi
 
